2fd0e541e32dae48f65a7a8726127cd2e37983c2
loribu <46202743+loribu@users.noreply.github.com>
2020-05-01 18:29:50 -0400

    mute

--- bot/cogs/mod.py
+++ bot/cogs/mod.py
@@ -15,12 +15,14 @@ class Mod(commands.Cog):
 
     # ban
     @commands.command()
+    @commands.has_guild_permissions(ban_members=True, administrator=True)
     async def ban(self, ctx, member: discord.Member, *, reason=None):
         await member.ban(reason=reason)
         await ctx.send(f'Banned {member.mention}')
 
     # unban
     @commands.command()
+    @commands.has_guild_permissions(ban_members=True, administrator=True)
     async def unban(self, ctx, *, member):
         banned_users = await ctx.guild.bans()
         member_name, member_discriminator = member.split('#')
@@ -35,30 +37,50 @@ class Mod(commands.Cog):
 
     # banlist
     @commands.command()
+    @commands.has_guild_permissions(ban_members=True, administrator=True)
     async def banlist(self, ctx):
         banned_users = await ctx.guild.bans()
 
-        if (banned_users != None):
+        if banned_users:
             await ctx.send("BANNED USERS:\n")
 
             for ban_entry in banned_users:
                 user = ban_entry.user
                 await ctx.send(f"{user}\n")
-
         else:
             await ctx.send("There are no banned users.")
 
     # kick
     @commands.command()
+    @commands.has_guild_permissions(kick_members=True, administrator=True)
     async def kick(self, ctx, member: discord.Member, *, reason=None):
         await member.kick(reason=reason)
 
-    # mute
+    # text mute
     @commands.command()
+    @commands.has_guild_permissions(mute_members=True, administrator=True)
     async def mute(self, ctx, member: discord.Member, *, reason=None):
-        await member.mute(reason=reason)
 
-    # unmute
+        role = discord.utils.get(ctx.guild.roles, name="Muted")
+
+        if not role:  # if role doesn't already exist, create it
+            muted = await ctx.guild.create_role(name="Muted")
+
+        for channel in ctx.guild.channels:  # removes permission to view and send in the channels
+            await channel.set_permissions(muted, send_messages=False,
+                                          read_messages=False)
+
+        await member.add_roles(muted)
+        if (reason == None):
+            await ctx.send(f'{member.mention} has been muted')
+        else:
+            await ctx.send(f'{member.mention} has been muted for reason: {reason}')
+
+    # text unmute
+    @commands.command()
+    @commands.has_guild_permissions(mute_members=True, administrator=True)
+    async def unmute(self, ctx, member: discord.Member):
+        await member.remove_roles(muted)
 
 
 def setup(client):
